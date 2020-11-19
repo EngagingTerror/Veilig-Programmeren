c78c2f40133cabb432db979ec42ab3ec59a6ffe2
EngagingTerror <54835045+EngagingTerror@users.noreply.github.com>
2020-11-19 10:50:15 +0100

    opdracht 3

--- db.php
+++ db.php
@@ -6,35 +6,35 @@ define("DB_USER", "root");
 define("DB_PASS", "");
 
 
-define("DB_NAME", "VP");
+define("DB_NAME", "brokenauth");
 
 class DB{
-	protected $dbh;
-	protected $stmt;
+    protected $dbh;
+    protected $stmt;
     protected $resultSet;
 
-	public function __construct(){
-		$this->dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+    public function __construct(){
+        $this->dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
         $this->resultSet = [];
-	}
+    }
 
-	public function executeSQL($query){
+    public function executeSQL($query){
         $this->stmt = $this->dbh->prepare($query);
-		$result = $this->stmt->execute();
+        $result = $this->stmt->execute();
 
-		if (!$result) {
-    		die('<pre>Oops,Error execute query '. $query .'</pre><br><pre>'.'Result code: '. $result .'</pre>');
-   		}
+        if (!$result) {
+            die('<pre> Error execute query '. $query .'</pre><br><pre>'.'Result code: '. $result .'</pre>');
+           }
 
-		$row = $this->stmt->fetchAll();
+        $row = $this->stmt->fetchAll();
 
-		if(!empty($row)){
-			$this->resultSet = $row;
-			return $this->resultSet;
-		}
+        if(!empty($row)){
+            $this->resultSet = $row;
+            return $this->resultSet;
+        }
 
-		return $this->resultSet;
-	}
+        return $this->resultSet;
+    }
 }
 
 ?>
